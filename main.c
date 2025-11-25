eb0c0c26d8af263ba16896f4fcc9abfd46668f60
Pholi658 <totipholiso@gmail.com>
2025-11-25 18:46:23 +0200

    fix parse()

--- main.c
+++ main.c
@@ -1,43 +1,41 @@
 #include "shell.h"
 
 char **env = NULL;
-char *command = NULL;
 
 /**
- * print_env - prints the environment variables to stdout
- *
- * Return: 0 on success, -1 if env is NULL
+ * print_env - prints the environment
+ * Return: 0 on success
  */
 int print_env(void)
 {
-	int i = 0;
+    int i = 0;
 
-	if (env == NULL)
-		return (-1);
+    if (!env)
+        return -1;
 
-	while (env[i])
-	{
-		printf("%s\n", env[i++]);
-	}
-	return (0);
+    while (env[i])
+        printf("%s\n", env[i++]);
+
+    return 0;
 }
 
 /**
- * parse - tokenizes the input command and calls execute
- * @command: command buffer (modified by strtok)
- * @envp: environment array
+ * parse - tokenizes command and executes
+ * @command: input line
+ * @envp: environment
  */
-void parse(char command[], char **envp)
+void parse(char *command, char **envp)
 {
     char *arguments[11];
-    char *token = NULL;
+    char *token;
     int arg_count = 0;
+    int status;
 
-    if (command == NULL)
+    if (!command || !*command)
         return;
 
     token = strtok(command, " ");
-    while (token != NULL && arg_count < 10)
+    while (token && arg_count < 10)
     {
         arguments[arg_count++] = token;
         token = strtok(NULL, " ");
@@ -51,92 +49,86 @@ void parse(char command[], char **envp)
 
     arguments[arg_count] = NULL;
 
-    if (arg_count > 0)
+    if (arg_count == 0)
+        return;
+
+    /* Check for built-ins */
+    if (strcmp(arguments[0], "exit") == 0)
     {
-        if (envp != NULL)
-            execute(arguments, envp);   /* <-- FIXED HERE */
-        else
-            fprintf(stderr, "./hsh: 1: %s: not found\n", arguments[0]);
+        exit(EXIT_SUCCESS);
     }
+    else if (strcmp(arguments[0], "env") == 0)
+    {
+        print_env();
+        return;
+    }
+
+    /* Execute command */
+    status = execute(arguments, envp);
+
+    /* Command not found handling */
+    if (status == 127)
+        exit(127);
 }
 
-
 /**
- * input - read a line from stdin, handle EOF and strip newline
- * @command: pointer to buffer (may be reallocated by getline)
+ * input - reads a line from stdin
+ * @command: pointer to buffer
  * @size: pointer to buffer size
  */
 void input(char **command, size_t *size)
 {
-	ssize_t read_bytes;
+    ssize_t read_bytes;
 
-	read_bytes = getline(command, size, stdin);
-	if (read_bytes == -1)
-	{
-		/* EOF or error */
-		if (isatty(STDIN_FILENO))
-			printf("\n");
-		if (*command != NULL)
-			free(*command);
-		exit(EXIT_SUCCESS);
-	}
-	if ((*command)[read_bytes - 1] == '\n')
-		(*command)[read_bytes - 1] = '\0';
+    read_bytes = getline(command, size, stdin);
+    if (read_bytes == -1)
+    {
+        if (isatty(STDIN_FILENO))
+            printf("\n");
+        if (*command)
+            free(*command);
+        exit(EXIT_SUCCESS);
+    }
+
+    if ((*command)[read_bytes - 1] == '\n')
+        (*command)[read_bytes - 1] = '\0';
 }
 
 /**
- * main - shell entrypoint (interactive or non-interactive)
+ * main - simple shell
  * @argc: arg count
  * @argv: arg vector
- * @envp: environment variables
- *
- * Return: 0 on success, or exit with failure
+ * @envp: environment
+ * Return: 0
  */
-int main(int argc, char *argv[], char **envp)
+int main(int argc, char *argv[], char *envp[])
 {
-	size_t size;
+    char *command = NULL;
+    size_t size = 0;
 
-	(void)argv;
-	env = envp;
-	command = NULL;
+    (void)argv;
+    env = envp;
 
-	if (argc > 1)
-	{
-		fprintf(stderr, "./shell: command does not exist\n");
-		exit(EXIT_FAILURE);
-	}
+    if (argc > 1)
+    {
+        fprintf(stderr, "./shell: command does not exist\n");
+        return EXIT_FAILURE;
+    }
 
-	while (1)
-	{
-		size = 0;
-		if (isatty(STDIN_FILENO))
-			printf("shell($) ");
-		input(&command, &size);
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+            printf("shell($) ");
+        fflush(stdout);
 
-		if (strcmp(command, "exit") == 0)
-		{
-			free(command);
-			exit(EXIT_SUCCESS);
-		}
+        input(&command, &size);
+        parse(command, envp);
 
-		if (strcmp(command, "env") == 0)
-		{
-			print_env();
-			free(command);
-			continue;
-		}
+        if (command)
+            free(command);
+        command = NULL;
+        size = 0;
+    }
 
-		/* If PATH isn't set and command is not an absolute path, it's not found */
-		if (_getenv("PATH", envp) == NULL && command[0] != '/')
-		{
-			fprintf(stderr, "./hsh: 1: %s: not found\n", command);
-			free(command);
-			continue;
-		}
-
-		parse(command, envp);
-		free(command);
-		command = NULL;
-	}
-	return (0);
+    return 0;
 }
